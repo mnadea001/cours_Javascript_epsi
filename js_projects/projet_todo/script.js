b8d3ba436cb8704124eb589e99c58b0cd43d26d6
Gladrat <3368584+Gladrat@users.noreply.github.com>
2023-01-16 09:28:58 +0100

    update

--- js_projects/projet_todo/script.js
+++ js_projects/projet_todo/script.js
@@ -1,2 +1,16 @@
-const taux_tva = 5.5
+const list = "#list"
+const input = "#input"
+const add = "#add"
+const clear = "#clear"
+const url = "#url"
+const load = "#load"
 
+const task = ["Coder en Python", "Coder en c++", "Coder TypeScript"]
+
+
+document.getElementById("mon-id")
+document.getElementsByClassName("toto")
+document.getElementsByTagName("")
+
+document.querySelector("")
+document.querySelectorAll("")
