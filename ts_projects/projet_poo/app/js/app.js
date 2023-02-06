33705af1b04cb893dd78d0657754deda6b7dc7f7
Gladrat <3368584+Gladrat@users.noreply.github.com>
2023-02-06 11:29:14 +0100

    update

--- ts_projects/projet_poo/app/js/app.js
+++ ts_projects/projet_poo/app/js/app.js
@@ -1,3 +1,50 @@
 "use strict";
-console.log("Hello world !");
+class Department {
+    constructor(id, name) {
+        this.id = id;
+        this.name = name;
+        this.employees = [];
+    }
+    describe() {
+        console.log(`Department: ${this.name}`);
+    }
+    addEmployee(employee) {
+        this.employees.push(employee);
+    }
+    printEmployees() {
+        console.log(`Employees nb: ${this.employees.length}`);
+        console.log(`Employees: ${this.employees}`);
+    }
+}
+class ITDepartment extends Department {
+    constructor(id, admins) {
+        super(id, "IT");
+        this.id = id;
+        this.admins = admins;
+        for (const admin of admins) {
+            this.addEmployee(admin);
+        }
+    }
+    printEmployees() {
+        super.printEmployees();
+        console.log(`Admins: ${this.admins}`);
+    }
+    countEmployees() {
+        console.log(`Employees nb: ${this.employees}`);
+    }
+    get adminsList() {
+        return this.admins;
+    }
+    set adminsList(admin) {
+        console.log("Vérification des droits d'accès ---- ok");
+        this.admins = admin;
+    }
+}
+const dept1 = new ITDepartment("00A", ["Max"]);
+console.log(dept1);
+dept1.describe();
+dept1.addEmployee("Helen");
+dept1.printEmployees();
+dept1.adminsList = ["Carol", "Tom"];
+dept1.printEmployees();
 //# sourceMappingURL=app.js.map
--- ts_projects/projet_poo/app/src/app.ts
+++ ts_projects/projet_poo/app/src/app.ts
@@ -1,1 +1,69 @@
-console.log("Hello world !")
+class Department {
+    // private name: string
+    protected employees: string[] = []
+
+    constructor(protected readonly id: string, protected name: string) {
+        // this.name = name
+    }
+
+    describe () {
+        console.log(`Department: ${this.name}`);
+    }
+
+    addEmployee(employee: string) {
+        // Gestion des droits
+        this.employees.push(employee)
+    }
+    
+    printEmployees() {
+        // Gestion des droits
+        console.log(`Employees nb: ${this.employees.length}`)
+        console.log(`Employees: ${this.employees}`)
+    }
+}
+
+class ITDepartment extends Department {
+    // superAdmin: string
+        // 1er admin de la liste
+        // Accesseur & mutateurs
+
+    constructor(protected id: string, private admins: string[]) {
+        super(id, "IT")
+        for (const admin of admins) {
+            this.addEmployee(admin)
+        }
+    }
+
+    printEmployees() {
+        super.printEmployees()
+        console.log(`Admins: ${this.admins}`);
+    }
+
+    countEmployees() {
+        console.log(`Employees nb: ${this.employees}`);
+        
+    }
+
+    get adminsList() {
+        // Gestion des droits d'accès
+        return this.admins
+    }
+
+    set adminsList(admin: string[]) {
+        console.log("Vérification des droits d'accès ---- ok");
+        
+        this.admins = admin
+    }
+}
+
+const dept1 = new ITDepartment("00A", ["Max"])
+console.log(dept1);
+dept1.describe()
+
+// dept1.addEmployee("Max")
+dept1.addEmployee("Helen")
+
+dept1.printEmployees()
+
+dept1.adminsList = ["Carol", "Tom"]
+dept1.printEmployees()
